cb61c63f2da5311c663fe343d0d394586ce17adc
isma297 <ismael.vega297@gmail.com>
2022-08-30 17:26:10 -0300

    change function changeColor to paint each cell a random color

--- script.js
+++ script.js
@@ -24,7 +24,9 @@ function drawGrid(squares){
 }
 
 function changeColor(square){
-  square.classList.add('active');
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  square.style.backgroundColor= "#" + randomColor;
+  
 }
 
 function getNumberOfSquares(){
