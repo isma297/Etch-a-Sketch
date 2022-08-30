87aefedf751f4d773d8db13cc7264a871988e2cf
isma297 <ismael.vega297@gmail.com>
2022-08-30 16:30:02 -0300

    add function to resize the cells inside the grid - change drawGrid function to change the number of columns in function of cells input

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-const container=document.querySelector('#grid');
-for(let i=0;i<16;i++){
-  let div=document.createElement('div');
-  div.setAttribute('class','square');
-  grid.appendChild(div);
+const grid=document.querySelector('#grid');
+const button=document.querySelector('#button');
+
+button.addEventListener('click',()=>{getNumberOfSquares()});
+
+
+function getSquareSize(squares){
+  let squareSize=grid.offsetWidth/squares;
+  return squareSize;
 }
-const divs=document.querySelectorAll('.square');
-divs.forEach((square)=>{square.addEventListener('mouseover',()=>{changeColor(square)})});
+
+function drawGrid(squares){
+  for(let i=0;i<squares**2;i++){
+    let div=document.createElement('div');
+    div.setAttribute('class','square');
+    div.style.width=getSquareSize(squares);
+    grid.appendChild(div);
+  }
+  grid.style.gridTemplateColumns=`repeat(${squares},1fr)`;
+
+  const divs=document.querySelectorAll('.square');
+  divs.forEach((square)=>{square.addEventListener('mouseover',
+  ()=>{changeColor(square)})});
+}
+
 function changeColor(square){
   square.classList.add('active');
 }
 
-
-const button=document.querySelector('#button');
-let squares=0;
-button.addEventListener('click',()=>{getNumberOfSquares()});
-function getNumberOfSquares(){ squares=prompt('how many squares?',)
-if (squares>100){getNumberOfSquares()}};
-
-
-
-
-
-// const container=document.querySelector('#container');
-// let div=document.createElement('div');
-// div.setAttribute('class','pixel');
-// container.appendChild(div);
-// div.addEventListener('mouseover',()=>{div.classList.toggle('active')})
+function getNumberOfSquares(){
+  let squares=prompt('how many squares?',)
+  if (squares>100){getNumberOfSquares()};
+  drawGrid(squares);
+};
