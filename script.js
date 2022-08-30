5ff0b78448d70054e49a9fd22b8368fc294e1424
isma297 <ismael.vega297@gmail.com>
2022-08-30 13:01:54 -0300

    changed variables names to more meaningfull names and fixed the problem where any div in the page got colored

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-const container=document.querySelector('#container');
+const container=document.querySelector('#grid');
 for(let i=0;i<16;i++){
   let div=document.createElement('div');
-  div.setAttribute('class','pixel');
-  container.appendChild(div);
+  div.setAttribute('class','square');
+  grid.appendChild(div);
 }
-const divs=document.querySelectorAll('div');
-divs.forEach((div)=>{div.addEventListener('mouseover',()=>{changeColor(div)})});
-function changeColor(div){
-  div.classList.toggle('active');
+const divs=document.querySelectorAll('.square');
+divs.forEach((square)=>{square.addEventListener('mouseover',()=>{changeColor(square)})});
+function changeColor(square){
+  square.classList.add('active');
 }
+
+
 const button=document.querySelector('#button');
 let squares=0;
 button.addEventListener('click',()=>{getNumberOfSquares()});
 function getNumberOfSquares(){ squares=prompt('how many squares?',)
 if (squares>100){getNumberOfSquares()}};
 
-div.setAttribute('style', 'grid-template-columns: `repeat(${squares},1fr)`; background-color:black;');
 
 
 
